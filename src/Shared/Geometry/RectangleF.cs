68712e8018d5aad48d9885f6f394ce880ba7dd6b
Scott MacDonald <scott@whitespaceconsideredharmful.com>
2013-02-05 21:11:23 -0600

    Commiting rectangle + unit tests

---
+++ src/Shared/Geometry/RectangleF.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace Scott.Geometry
+{
+    /// <summary>
+    /// Represents a rectangle using a floating point values
+    /// </summary>
+    public struct RectangleF
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+
+        public static readonly RectangleF Empty = new RectangleF( 0.0f, 0.0f, 0.0f, 0.0f );
+
+        public float Top
+        {
+            get
+            {
+                return Y;
+            }
+        }
+
+        public float Left
+        {
+            get
+            {
+                return X;
+            }
+        }
+
+        public float Bottom
+        {
+            get
+            {
+                return Y + Height;
+            }
+        }
+
+        public float Right
+        {
+            get
+            {
+                return X + Width;
+            }
+        }
+
+        public Vector2 Location
+        {
+            get
+            {
+                return new Vector2( X, Y );
+            }
+        }
+
+        public Vector2 Size
+        {
+            get
+            {
+                return new Vector2( Width, Height );
+            }
+        }
+
+        /// <summary>
+        ///  Tests if the rectangle has a zero width or height.
+        /// </summary>
+        public bool IsEmpty
+        {
+            get
+            {
+                return Width == 0.0f || Height == 0.0f;
+            }
+        }
+
+        /// <summary>
+        ///  Rectangle constructor
+        /// </summary>
+        /// <param name="x">Upper x coordinate</param>
+        /// <param name="y">Upper y coordinate</param>
+        /// <param name="width">Width of the rectangle</param>
+        /// <param name="height">Height of the rectangle</param>
+        public RectangleF( float x, float y, float width, float height )
+            : this()
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+    }
+}
---
+++ src/Shared/Geometry/Tests/RectangleFTests.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.Xna.Framework;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scott.Geometry.Tests
+{
+    /// <summary>
+    /// Tests for RectangleF
+    /// </summary>
+    [TestFixture]
+    [Category("Common/Geometry")]
+    internal class RectangleFTests
+    {
+        [Test]
+        public void DefaultRect()
+        {
+            RectangleF rect = default(RectangleF);
+
+            Assert.AreEqual( 0.0f, rect.X );
+            Assert.AreEqual( 0.0f, rect.Y );
+            Assert.AreEqual( 0.0f, rect.Width );
+            Assert.AreEqual( 0.0f, rect.Height );
+        }
+
+        [Test]
+        public void CreateWithPointAndDimensions()
+        {
+            RectangleF rect = new RectangleF( 2.5f, 3.0f, 4.5f, 6.2f );
+
+            Assert.AreEqual( 2.5f, rect.X );
+            Assert.AreEqual( 3.0f, rect.Y );
+            Assert.AreEqual( 4.5f, rect.Width );
+            Assert.AreEqual( 6.2f, rect.Height );
+        }
+
+        [Test]
+        public void IsRectEmpty()
+        {
+            // both width and height are empty
+            RectangleF rectA = new RectangleF( 2.5f, -3.0f, 0.0f, 0.0f );
+            Assert.IsTrue( rectA.IsEmpty );
+
+            // only width is zero
+            RectangleF rectB = new RectangleF( 2.5f, -3.0f, 0.0f, 1.0f );
+            Assert.IsTrue( rectB.IsEmpty );
+
+            // only height is zero
+            RectangleF rectC = new RectangleF( 2.5f, -3.0f, 1.0f, 0.0f );
+            Assert.IsTrue( rectC.IsEmpty );
+
+            // neither width nor height is zero
+            RectangleF rectD = new RectangleF( 2.5f, -3.0f, 1.0f, 1.0f );
+            Assert.IsFalse( rectD.IsEmpty );
+        }
+
+        [Test]
+        public void DefaultRectIsEmpty()
+        {
+            RectangleF rect = default( RectangleF );
+            Assert.IsTrue( rect.IsEmpty );
+        }
+
+        [Test]
+        public void EmptyRectFieldIsEmpty()
+        {
+            RectangleF rect = RectangleF.Empty;
+            Assert.IsTrue( rect.IsEmpty );
+        }
+
+        [Test]
+        public void X()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            Assert.AreEqual( 2.5f, rect.X );
+        }
+
+        [Test]
+        public void Y()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            Assert.AreEqual( -3.0f, rect.Y );
+        }
+
+        [Test]
+        public void SetX()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            rect.X = 7.0f;
+            Assert.AreEqual( 7.0f, rect.X );
+        }
+
+        [Test]
+        public void SetY()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            rect.Y = 9.0f;
+            Assert.AreEqual( 9.0f, rect.Y );
+        }
+
+        [Test]
+        public void Width()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            Assert.AreEqual( 1.2f, rect.Width );
+        }
+
+        [Test]
+        public void Height()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            Assert.AreEqual( 0.5f, rect.Height );
+        }
+
+        [Test]
+        public void SetWidth()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            rect.Width = 7.0f;
+            Assert.AreEqual( 7.0f, rect.Width );
+        }
+
+        [Test]
+        public void SetHeight()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            rect.Height = 9.0f;
+            Assert.AreEqual( 9.0f, rect.Height );
+        }
+
+        [Test]
+        public void Top()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            Assert.AreEqual( -3.0f, rect.Top );
+        }
+
+        [Test]
+        public void Left()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            Assert.AreEqual( 2.5f, rect.Left );
+        }
+
+        [Test]
+        public void Bottom()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            Assert.AreEqual( -2.5f, rect.Bottom );
+        }
+
+        [Test]
+        public void Right()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            Assert.AreEqual( 3.7f, rect.Right );
+        }
+
+        [Test]
+        public void Location()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            Vector2 location = rect.Location;
+
+            Assert.AreEqual( 2.5f, location.X );
+            Assert.AreEqual( -3.0f, location.Y );
+        }
+
+        [Test]
+        public void Size()
+        {
+            RectangleF rect = new RectangleF( 2.5f, -3.0f, 1.2f, 0.5f );
+            Vector2 size = rect.Size;
+
+            Assert.AreEqual( 1.2f, size.X );
+            Assert.AreEqual( 0.5f, size.Y );
+        }
+    }
+}
