ad04de8779750ce931c4025f2233f04c9524782c
Scott MacDonald <scott@whitespaceconsideredharmful.com>
2013-02-09 18:07:59 -0600

    ParentableNode initial commit
    Base for game objects

---
+++ src/Shared/Common/ParentableNode.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scott.Common
+{
+    public class ChildChangedEventArgs : EventArgs
+    {
+        public Object Child { get; set; }
+    }
+
+    public class ParentChangedEventArgs : EventArgs
+    {
+        public Object PreviousOwner { get; set; }
+        public Object Owner { get; set; }
+    }
+
+    /// <summary>
+    /// MAKE SURE TO IMPLEMENT EQUALS PROPERLY
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ParentableNode<T> where T : class
+    {
+        /// <summary>
+        ///  Gets the parent of this object.
+        /// </summary>
+        public ParentableNode<T> Parent
+        {
+            get
+            {
+                return mParent;
+            }
+            set
+            {
+                if ( value == null )
+                {
+                    RemoveParent();
+                }
+                else
+                {
+                    SetParent( value );
+                }
+            }
+        }
+
+        /// <summary>
+        ///  Gets the sibling object immediately following this object.
+        /// </summary>
+        public ParentableNode<T> NextSibling
+        {
+            get
+            {
+                return mNextSibling;
+            }
+        }
+
+        /// <summary>
+        ///  Gets the sibling object immediately prior to this object.
+        /// </summary>
+        public ParentableNode<T> PreviousSibling
+        {
+            get
+            {
+                return mPreviousSibling;
+            }
+        }
+
+        /// <summary>
+        ///  Gets the first child of this object.
+        /// </summary>
+        public ParentableNode<T> FirstChild
+        {
+            get
+            {
+                return mFirstChild;
+            }
+        }
+
+        /// <summary>
+        ///  Gets the last child of this object.
+        /// </summary>
+        public ParentableNode<T> LastChild
+        {
+            get
+            {
+                return mLastChild;
+            }
+        }
+
+        /// <summary>
+        ///  Gets an enumerable list of this object's children.
+        /// </summary>
+        public IEnumerable<T> Children
+        {
+            get
+            {
+                ParentableNode<T> node = mFirstChild;
+
+                while ( node != null )
+                {
+                    yield return node.mValue;
+                    node = mFirstChild.mNextSibling;
+                }
+            }
+        }
+
+        /// <summary>
+        ///  Gets the number of children belonging to this object.
+        /// </summary>
+        public int ChildrenCount
+        {
+            get
+            {
+                return mChildCount;
+            }
+        }
+
+        /// <summary>
+        ///  Gets a value indicating whether this object has a parent.
+        /// </summary>
+        public bool HasParent
+        {
+            get
+            {
+                return ( mParent != null );
+            }
+        }
+
+        /// <summary>
+        ///  Gets a value indicating whether this object has any children.
+        /// </summary>
+        public bool HasChildren
+        {
+            get
+            {
+                return ( mFirstChild != null );
+            }
+        }
+
+        private ParentableNode<T> mParent = null;
+        private ParentableNode<T> mNextSibling = null;
+        private ParentableNode<T> mPreviousSibling = null;
+        private ParentableNode<T> mFirstChild = null;
+        private ParentableNode<T> mLastChild = null;
+        private T mValue = default( T );
+
+        private int mChildCount = 0;
+
+        /// <summary>
+        ///  Default constructor.
+        /// </summary>
+        public ParentableNode()
+            : this( null )
+        {
+        }
+
+        /// <summary>
+        ///  ParentableObject constructor.
+        /// </summary>
+        public ParentableNode( ParentableNode<T> parent )
+        {
+            if ( parent != null )
+            {
+                SetParent( parent );
+            }
+        }
+
+        /// <summary>
+        ///  Checks if the child exists in this object's list of children.
+        /// </summary>
+        public bool ContainsChild( ParentableNode<T> childObject )
+        {
+            // Search for the child in our list of children
+            ParentableNode<T> node = mFirstChild;
+
+            while ( node != null )
+            {
+                if ( node == childObject )
+                {
+                    break;
+                }
+                else
+                {
+                    node = node.mNextSibling;
+                }
+            }
+
+            // Were we able to locate it?
+            return ( node != null );
+        }
+
+        /// <summary>
+        ///  Checks if this object is a child of the parent object.
+        /// </summary>
+        public bool IsChildOf( ParentableNode<T> parent, bool searchParentsRecursively = false )
+        {
+            if ( mParent == parent )
+            {
+                return true;
+            }
+            else if ( mParent != null && searchParentsRecursively )
+            {
+                return mParent.IsChildOf( parent, true );
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        ///  Sets this object's parent.
+        /// </summary>
+        public void SetParent( ParentableNode<T> newParent )
+        {
+            // Ensure that the caller is not trying to add this object as its parent, which will lead
+            // quite happily into an infinte loop.
+            if ( ReferenceEquals( this, newParent ) )
+            {
+                throw new ParentingException( "Cannot parent object to itself" );
+            }
+
+            // Detach the object from it's current parent, and sever all child / sibling links first.
+            RemoveParent();
+
+            // Now attach ourself to the new parent. If the new parent is null, well our work is done
+            // :)
+            if ( newParent != null )
+            {
+                mParent = newParent;
+                mParent.mChildCount++;
+
+                // Place ourself into the parent's list of children by making the parent's last child
+                // point to us as the next sibling.
+                if ( newParent.mLastChild != null )
+                {
+                    mPreviousSibling = newParent.mLastChild;
+                    mPreviousSibling.mNextSibling = this;
+                }
+                else
+                {
+                    // The parent's last child reference is null, which means this parent has no
+                    // children. Configure the FirstChild reference to point to us.
+                    newParent.mFirstChild = this;
+                }
+
+                // Set this object as the last sibling.
+                newParent.mLastChild = this;
+            }
+        }
+
+        /// <summary>
+        ///  Disconnect this object from it's parent by setting the parent to null.
+        /// </summary>
+        private void RemoveParent()
+        {
+            // If this object has a parent, go ahead and remove this object from the parent's linked
+            // list of children.
+            if ( mParent != null )
+            {
+                if ( mParent.mFirstChild != null && ReferenceEquals( mParent.mFirstChild, this ) )
+                {
+                    mParent.mFirstChild = mParent.mFirstChild.mNextSibling;
+                }
+
+                if ( mParent.mLastChild != null && ReferenceEquals( mParent.mLastChild, this ) )
+                {
+                    mParent.mLastChild = mParent.mLastChild.mPreviousSibling;
+                }
+
+                mParent.mChildCount--;
+                mParent = null;
+            }
+
+            // If this object has siblings, we will need to remove ourself from that linked list as
+            // well.
+            if ( mNextSibling != null )
+            {
+                mNextSibling.mPreviousSibling = mPreviousSibling;
+            }
+
+            if ( mPreviousSibling != null )
+            {
+                mPreviousSibling.mNextSibling = mNextSibling;
+            }
+
+            mNextSibling = null;
+            mPreviousSibling = null;
+        }
+
+        /// <summary>
+        ///  Adds the child to the end of this object's list of children.
+        /// </summary>
+        public void AddChild( ParentableNode<T> child )
+        {
+            // Refuse to add a null child
+            if ( child == null )
+            {
+                throw new ParentingException( "Cannot add a null child" );
+            }
+
+            // Refuse to add this object as it's child
+            if ( ReferenceEquals( child, this ) )
+            {
+                throw new ParentingException( "Cannot add self as a child" );
+            }
+
+            child.SetParent( this );
+        }
+
+        /// <summary>
+        ///  Removes the child from this object's list of children which will cause
+        ///  the child to become parentless and sibling-less.
+        /// </summary>
+        public void RemoveChild( ParentableNode<T> child )
+	    {
+		    if ( child == null )
+		    {
+			    throw new ParentingException( "Cannot remove a null child" );
+		    }
+
+            if ( ReferenceEquals( child, this ) )
+            {
+                throw new ParentingException( "Cannot remove self as a child" );
+            }
+
+            // Find the child requested
+            ParentableNode<T> node = mFirstChild;
+
+            while ( node != null && !ReferenceEquals( node, child ) )
+            {
+                node = node.mNextSibling;
+            }
+
+		    // Did we find the child node? If so, unparent it from this object
+            if ( node != null )
+            {
+                node.SetParent( null );
+            }
+	    }
+    }
+}
---
+++ src/Shared/Common/Tests/ParentableNodeTests.cs
@@ -0,0 +1,191 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scott.Common.Tests
+{
+    [TestFixture]
+    [Category( "Common" )]
+    class ParentableNodeTests
+    {
+        class TestObject : ParentableNode<TestObject>
+        {
+            public string Value { get; set; }
+
+            public TestObject( string v )
+                : base()
+            {
+                Value = v;
+            }
+
+            public TestObject( string v, TestObject obj )
+                : base( obj )
+            {
+                Value = v;
+            }
+        }
+
+        [Test]
+        public void TestDefaultConstructor()
+        {
+            TestObject a = new TestObject( "a" );
+
+            Assert.AreEqual( "a", a.Value );
+
+            Assert.IsNull( a.Parent );
+            Assert.IsNull( a.FirstChild );
+            Assert.IsNull( a.LastChild );
+            Assert.IsNull( a.PreviousSibling );
+            Assert.IsNull( a.NextSibling );
+
+            Assert.AreEqual( 0, a.ChildrenCount );
+        }
+
+        [Test]
+        public void TestConstructorWithParent()
+        {
+            TestObject a = new TestObject( "a" );
+            TestObject b = new TestObject( "b", a );
+
+            // Test A
+            Assert.IsNull( a.Parent );
+            Assert.IsNotNull( a.FirstChild );
+            Assert.IsNotNull( a.LastChild );
+            Assert.IsNull( a.PreviousSibling );
+            Assert.IsNull( a.NextSibling );
+
+            Assert.AreSame( b, a.FirstChild );
+            Assert.AreSame( b, a.LastChild );
+
+            Assert.AreEqual( 1, a.ChildrenCount );
+
+            // Test B
+            Assert.AreSame( a, b.Parent );
+
+            Assert.IsNotNull( b.Parent );
+            Assert.IsNull( b.FirstChild );
+            Assert.IsNull( b.LastChild );
+            Assert.IsNull( b.PreviousSibling );
+            Assert.IsNull( b.NextSibling );
+
+            Assert.AreEqual( 0, b.ChildrenCount );
+        }
+
+        [Test]
+        public void TestParentProperty()
+        {
+            TestObject a = new TestObject( "a" );
+            TestObject b = new TestObject( "b" );
+
+            // b has no parent yet
+            Assert.IsNull( b.Parent );
+
+            // now a has a parent
+            b.SetParent( a );
+
+            Assert.IsNotNull( b.Parent );
+        }
+
+        [Test]
+        public void TestNextPreviousSiblingProperty()
+        {
+            TestObject a = new TestObject( "a" );
+            TestObject b = new TestObject( "b" );
+
+            // b has no siblings yet
+            Assert.IsNull( b.NextSibling );
+            Assert.IsNull( b.PreviousSibling );
+
+            // b still has no siblings
+            b.SetParent( a );
+            Assert.IsNull( b.NextSibling );
+            Assert.IsNull( b.PreviousSibling );
+
+            // add another child 'c' to 'a'.
+            //    a
+            //   / \
+            //  b - c
+            TestObject c = new TestObject( "c", a );
+
+            Assert.IsNotNull( b.NextSibling );
+            Assert.AreSame( c, b.NextSibling );
+            Assert.IsNull( b.PreviousSibling );
+
+            Assert.IsNull( c.NextSibling );
+            Assert.IsNotNull( c.PreviousSibling );
+            Assert.AreSame( b, c.PreviousSibling );
+
+            // add another child 'd' to 'a'
+            //    a
+            //   /
+            //   b - c - d
+            TestObject d = new TestObject( "d", a );
+
+            Assert.AreSame( c, b.NextSibling );
+            Assert.IsNull( b.PreviousSibling );
+
+            Assert.AreSame( d, c.NextSibling );
+            Assert.AreSame( b, c.PreviousSibling );
+
+            Assert.IsNull( d.NextSibling );
+            Assert.AreSame( c, d.PreviousSibling );
+        }
+
+        [Test]
+        public void TestFirstChildProperty()
+        {
+            TestObject a = new TestObject( "a" );
+            Assert.IsNull( a.FirstChild );
+
+            TestObject b = new TestObject( "b", a );
+            Assert.AreSame( b, a.FirstChild );
+
+            TestObject c = new TestObject( "c", a );
+            Assert.AreSame( b, a.FirstChild );
+
+            TestObject d = new TestObject( "d", a );
+            Assert.AreSame( b, a.FirstChild );
+        }
+
+        [Test]
+        public void TestLastChildProperty()
+        {
+            TestObject a = new TestObject( "a" );
+            Assert.IsNull( a.LastChild );
+
+            TestObject b = new TestObject( "b", a );
+            Assert.AreSame( b, a.LastChild );
+
+            TestObject c = new TestObject( "c", a );
+            Assert.AreSame( c, a.LastChild );
+
+            TestObject d = new TestObject( "d", a );
+            Assert.AreSame( d, a.LastChild );
+        }
+
+        [Test]
+        public void TestChildrenProperty()
+        {
+ /*           List<TestObject> list;
+
+            TestObject a = new TestObject( "a" );
+            list = new List<TestObject>( a.Children );
+
+            Assert.AreEqual( 0, list.Count );
+
+            TestObject b = new TestObject( "b", a );
+            list = new List<TestObject>( a.Children );
+
+            Assert.AreEqual( 1, list.Count );
+            Assert.AreSame( a, list[0] ); ;
+
+            TestObject c = new TestObject( "c", a );
+            Assert.AreSame( c, a.LastChild );
+
+            TestObject d = new TestObject( "d", a );
+            Assert.AreSame( d, a.LastChild );*/
+        }
+    }
+}
