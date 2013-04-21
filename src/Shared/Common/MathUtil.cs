fb85f5b8752b64ba9871867c423800ac663058ed
Scott MacDonald <scott@whitespaceconsideredharmful.com>
2013-04-21 18:32:12 -0500

    Adding utility math functions with unit tests

--- src/Shared/Common/MathUtil.cs
+++ src/Shared/Common/MathUtil.cs
@@ -1,9 +1,28 @@
-﻿namespace Scott.Common
+﻿/*
+ * MathUtil.cs
+ * Copyright 2012-2013 Scott MacDonald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+
+namespace Scott.Common
 {
     /// <summary>
-    ///  Common math utility functions.
+    ///  Extra math utilities that are not normally found in System.Math.
     /// </summary>
-    public static class MathUtil
+    public static class MathX
     {
         /// <summary>
         ///  Normalize a value in the [min,max] range to [0.0,1.0]. Range must be zero or greater.
@@ -51,6 +70,259 @@
         public static double NormalizeToNegativeOneOneRange( double v, double min, double max )
         {
             return 2.0 * ( v - min ) / ( max - min ) + -1.0;
+        }
+
+        /// <summary>
+        ///  Convert angle in degrees to angle in radians.
+        /// </summary>
+        /// <param name="angle">Angle in degrees.</param>
+        /// <returns>The angle in radians.</returns>
+        public static float DegreeToRadian( float angle )
+        {
+            return (float) Math.PI * angle / 180.0f;
+        }
+
+        /// <summary>
+        ///  Convert angle in radians to angle in degrees.
+        /// </summary>
+        /// <param name="angle">Angle in radians.</param>
+        /// <returns>The angle in degrees.</returns>
+        public static float RadianToDegree( float angle )
+        {
+            return angle * ( 180.0f / (float) Math.PI );
+        }
+
+        /// <summary>
+        ///  Returns the smallest difference between two angle values in radians.
+        /// </summary>
+        /// <remarks>
+        ///  This method requires the values a and b to be constrained to [0, 2PI].
+        ///  Credit: http://gamedev.stackexchange.com/a/4472
+        /// </remarks>
+        /// <param name="a">Radian angle between 0 and 2 * PI.</param>
+        /// <param name="b">Radian angle between 0 and 2 * PI.</param>
+        /// <returns>Smallest difference between a and b.</returns>
+        public static float SmallestAngleBetween( float a, float b )
+        {
+            return (float) Math.PI - Math.Abs( Math.Abs( a - b ) - (float) Math.PI );
+        }
+
+        /// <summary>
+        ///  Returns the smallest difference between two angle values in degrees.
+        /// </summary>
+        /// <remarks>
+        ///  This method requires the values a and b to be constrained to [0, 360].
+        ///  Credit: http://gamedev.stackexchange.com/a/4472
+        /// </remarks>
+        /// <param name="a">Degree angle between 0 and 360.</param>
+        /// <param name="b">Degree angle between 0 and 360.</param>
+        /// <returns>Smallest difference between a and b.</returns>
+        public static float SmallestDegreeAngleBetween( float a, float b )
+        {
+            return 180.0f - Math.Abs( Math.Abs( a - b ) - 180.0f );
+        }
+
+        /// <summary>
+        ///  Check if two double values are nearly equal.
+        /// </summary>
+        /// <param name="a">First value to check.</param>
+        /// <param name="b">Second value to check.</param>
+        /// <param name="epsilon">Maximal difference before they are not equal.</param>
+        /// <returns>If two double values are nearly equal.</returns>
+        public static bool NearlyEqual( double a, double b, double epsilon )
+        {
+            double absA = Math.Abs( a );
+            double absB = Math.Abs( b );
+            double diff = Math.Abs( a - b );
+
+            if ( a * b == 0.0 )
+            {
+                // A or b (or both) are zero. Relative error is therefore not meaningful here.
+                return diff < ( epsilon * epsilon );
+            }
+            else
+            {
+                // Use relative error.
+                return diff / ( absA + absB ) < epsilon;
+            }
+        }
+
+        /// <summary>
+        ///  Check if two floating point values are nearly equal.
+        /// </summary>
+        /// <param name="a">First value to check.</param>
+        /// <param name="b">Second value to check.</param>
+        /// <param name="epsilon">Maximal difference before they are not equal.</param>
+        /// <returns>If two floating point values are nearly equal.</returns>
+        public static bool NearlyEqual( float a, float b, float epsilon )
+        {
+            float absA = Math.Abs( a );
+            float absB = Math.Abs( b );
+            float diff = Math.Abs( a - b );
+
+            if ( a * b == 0.0f )
+            {
+                // A or b (or both) are zero. Relative error is therefore not meaningful here.
+                return diff < ( epsilon * epsilon );
+            }
+            else
+            {
+                // Use relative error.
+                return diff / ( absA + absB ) < epsilon;
+            }
+        }
+
+        /// <summary>
+        ///  Calculate an approximated sin value using faster math than calling Math.Sin. X must
+        ///  range between [0,PI].
+        /// </summary>
+        /// <remarks>
+        ///  Due to C# having so-so math performance with JIT, it appears that using Math.Sin is
+        ///  either just as fast, or ever so slightly slower than this method. It's probably not
+        ///  worth using this method, but I'm keeping it around for historical purposes.
+        ///  Credit: http://devmaster.net/forums/topic/4648-fast-and-accurate-sinecosine/
+        /// </remarks>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static float FastSin( float x )
+        {
+            const float B = 4.0f / (float) Math.PI;
+            const float C = -4.0f / (float) ( Math.PI * Math.PI );
+            const float P = 0.225f;
+
+            float y0 = B * x + C * x * Math.Abs( x );
+            float y1 = P * ( y0 * Math.Abs( y0 ) - y0 ) + y0;      // Q * y + P * y * abs(y)
+
+            return y1;
+        }
+
+        /// <summary>
+        ///  Return the next highest power of two that is larger than the given value.
+        /// </summary>
+        /// <param name="value">The minimum value.</param>
+        /// <returns>The next power of two larger than the given value.</returns>
+        public static long NextPowerOf2( long value )
+        {
+            return (long) NextPowerOf2( (ulong) value );
+        }
+
+        /// <summary>
+        ///  Return the next highest power of two that is larger than the given value.
+        /// </summary>
+        /// <param name="value">The minimum value.</param>
+        /// <returns>The next power of two larger than the given value.</returns>
+        public static int NextPowerOf2( int value )
+        {
+            return (int) NextPowerOf2( (uint) value );
+        }
+
+        /// <summary>
+        ///  Return the next highest power of two that is larger than the given value.
+        /// </summary>
+        /// <param name="value">The minimum value.</param>
+        /// <returns>The next power of two larger than the given value.</returns>
+        public static ulong NextPowerOf2( ulong value )
+        {
+            if ( value == 0 )
+            {
+                return 1;
+            }
+
+            value--;
+            value |= value >> 1;
+            value |= value >> 2;
+            value |= value >> 4;
+            value |= value >> 8;
+            value |= value >> 16;
+            value |= value >> 32;
+            value++;
+
+            return value;
+        }
+
+        /// <summary>
+        ///  Return the next highest power of two that is larger than the given value.
+        /// </summary>
+        /// <param name="value">The minimum value.</param>
+        /// <returns>The next power of two larger than the given value.</returns>
+        public static uint NextPowerOf2( uint value )
+        {
+            if ( value == 0 )
+            {
+                return 1;
+            }
+
+            value--;
+            value |= value >> 1;
+            value |= value >> 2;
+            value |= value >> 4;
+            value |= value >> 8;
+            value |= value >> 16;
+            value++;
+
+            return value;
+        }
+
+        /// <summary>
+        ///  Linearly interpolate a value between [a,b] by an amount defined by t. T must vary
+        ///  between zero and one.
+        /// </summary>
+        /// <remarks>
+        ///  This function will perform a smooth linear interpolation of the range defined by
+        ///  [a,b]. When t = 0, the value returned will be a. When t = 0.5, the value returned
+        ///  will be halfway between a and b. And when t = 1, the value returned will be b.
+        /// </remarks>
+        /// <param name="a">Starting value.</param>
+        /// <param name="b">Ending value.</param>
+        /// <param name="t">Interpolation amount, clamped to [0,1].</param>
+        /// <returns>The interpolated value.</returns>
+        public static float Lerp( float a, float b, float t )
+        {
+            return a + t * ( b - a );
+        }
+
+        /// <summary>
+        ///  Linearly interpolate a value between [a,b] by an amount defined by t. T must vary
+        ///  between zero and one.
+        /// </summary>
+        /// <remarks>
+        ///  This function will perform a smooth linear interpolation of the range defined by
+        ///  [a,b]. When t = 0, the value returned will be a. When t = 0.5, the value returned
+        ///  will be halfway between a and b. And when t = 1, the value returned will be b.
+        /// </remarks>
+        /// <param name="a">Starting value.</param>
+        /// <param name="b">Ending value.</param>
+        /// <param name="t">Interpolation amount, clamped to [0,1].</param>
+        /// <returns>The interpolated value.</returns>
+        public static double Lerp( double a, double b, double t )
+        {
+            return a + t * ( b - a );
+        }
+
+        /// <summary>
+        ///  Returns the interpolation factor used to interpolate between two values. This is the
+        ///  inverse of Lerp.
+        /// </summary>
+        /// <param name="a">Starting value.</param>
+        /// <param name="b">Ending value.</param>
+        /// <param name="t">Interpolate value from Lerp.</param>
+        /// <returns>The interpolation amount, clamped to [0,1].</returns>
+        public static float Unlerp( float a, float b, float t )
+        {
+            return ( t - a ) / ( b - a );
+        }
+
+        /// <summary>
+        ///  Returns the interpolation factor used to interpolate between two values. This is the
+        ///  inverse of Lerp.
+        /// </summary>
+        /// <param name="a">Starting value.</param>
+        /// <param name="b">Ending value.</param>
+        /// <param name="t">Interpolate value from Lerp.</param>
+        /// <returns>The interpolation amount, clamped to [0,1].</returns>
+        public static double Unlerp( double a, double b, double t )
+        {
+            return ( t - a ) / ( b - a );
         }
     }
 }
---
+++ src/Shared/Common/Tests/MathXTests.cs
@@ -0,0 +1,242 @@
+﻿
+#pragma warning disable 1591            // Disable all XML Comment warnings in this file
+
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scott.Common.Tests
+{
+    [TestFixture]
+    public class MathXTests
+    {
+        [Test]
+        public void NormalizeToZeroOneRange_Float()
+        {
+            // 0/4
+            Assert.AreEqual( 0.00f, MathX.NormalizeToZeroOneRange( 0.0f, 0.0f, 4.0f ) );
+
+            // 1/4
+            Assert.AreEqual( 0.25f, MathX.NormalizeToZeroOneRange( 1.0f, 0.0f, 4.0f ) );
+
+            // 2/4
+            Assert.AreEqual( 0.50f, MathX.NormalizeToZeroOneRange( 2.0f, 0.0f, 4.0f ) );
+
+            // 3/4
+            Assert.AreEqual( 0.75f, MathX.NormalizeToZeroOneRange( 3.0f, 0.0f, 4.0f ) );
+
+            // 4/4
+            Assert.AreEqual( 1.00f, MathX.NormalizeToZeroOneRange( 4.0f, 0.0f, 4.0f ) );
+
+            // 5/4
+            Assert.AreEqual( 1.25f, MathX.NormalizeToZeroOneRange( 5.0f, 0.0f, 4.0f ) );
+        }
+
+        [Test]
+        public void NormalizeToZeroOneRange_Double()
+        {
+            // 0/4
+            Assert.AreEqual( 0.00, MathX.NormalizeToZeroOneRange( 0.0, 0.0, 4.0 ) );
+
+            // 1/4
+            Assert.AreEqual( 0.25, MathX.NormalizeToZeroOneRange( 1.0, 0.0, 4.0 ) );
+
+            // 2/4
+            Assert.AreEqual( 0.50, MathX.NormalizeToZeroOneRange( 2.0, 0.0, 4.0 ) );
+
+            // 3/4
+            Assert.AreEqual( 0.75, MathX.NormalizeToZeroOneRange( 3.0, 0.0, 4.0 ) );
+
+            // 4/4
+            Assert.AreEqual( 1.00, MathX.NormalizeToZeroOneRange( 4.0, 0.0, 4.0 ) );
+
+            // 5/4
+            Assert.AreEqual( 1.25, MathX.NormalizeToZeroOneRange( 5.0, 0.0, 4.0 ) );
+        }
+
+        [Test]
+        public void DegreesToRadians()
+        {
+            Assert.AreEqual( 0.0f, MathX.DegreeToRadian( 0.0f ) );
+            Assert.AreEqual( 0.785398163f, MathX.DegreeToRadian( 45.0f ) );
+            Assert.AreEqual( 1.57079633f, MathX.DegreeToRadian( 90.0f ) );
+            Assert.AreEqual( 3.14159265f, MathX.DegreeToRadian( 180.0f ) );
+            Assert.AreEqual( 6.28318531f, MathX.DegreeToRadian( 360.0f ) );
+        }
+
+        [Test]
+        public void RadiansToDegrees()
+        {
+            Assert.AreEqual( 0.0f, MathX.RadianToDegree( 0.0f ) );
+            Assert.AreEqual( 45.0f, MathX.RadianToDegree( 0.785398163f ) );
+            Assert.AreEqual( 90.0f, MathX.RadianToDegree( 1.57079633f ) );
+            Assert.AreEqual( 180.0f, MathX.RadianToDegree( 3.14159265f ) );
+            Assert.AreEqual( 360.0f, MathX.RadianToDegree( 6.28318531f ) );
+        }
+
+        [Test]
+        public void SmallestAngleBetweenRadians()
+        {
+            float zero = MathX.DegreeToRadian( 0.0f );
+            float p10 = MathX.DegreeToRadian( 10.0f );
+            float p40 = MathX.DegreeToRadian( 40.0f );
+            float p50 = MathX.DegreeToRadian( 50.0f );
+            float p90 = MathX.DegreeToRadian( 90.0f );
+            float p350 = MathX.DegreeToRadian( 350.0f );
+            float p360 = MathX.DegreeToRadian( 360.0f );
+
+            Assert.AreEqual( zero, MathX.SmallestAngleBetween( 0.0f, 0.0f ) );
+            Assert.AreEqual( zero, MathX.SmallestAngleBetween( zero, p360 ), 0.00001f );
+            Assert.AreEqual( p10, MathX.SmallestAngleBetween( p40, p50 ), 0.00001f );
+            Assert.AreEqual( p40, MathX.SmallestAngleBetween( p50, p90 ), 0.00001f );
+            Assert.AreEqual( p10, MathX.SmallestAngleBetween( p350, p360 ), 0.00001f );
+            Assert.AreEqual( p50, MathX.SmallestAngleBetween( p350, p40 ), 0.00001f );
+        }
+
+        [Test]
+        public void SmallestAngleBetweenDegrees()
+        {
+            Assert.AreEqual( 0.0f, MathX.SmallestDegreeAngleBetween( 0.0f, 0.0f ) );
+            Assert.AreEqual( 0.0f, MathX.SmallestDegreeAngleBetween( 0.0f, 360.0f ), 0.00001f );
+            Assert.AreEqual( 10.0f, MathX.SmallestDegreeAngleBetween( 40.0f, 50.0f ), 0.00001f );
+            Assert.AreEqual( 40.0f, MathX.SmallestDegreeAngleBetween( 50.0f, 90.0f ), 0.00001f );
+            Assert.AreEqual( 10.0f, MathX.SmallestDegreeAngleBetween( 350.0f, 360.0f ), 0.00001f );
+            Assert.AreEqual( 50.0f, MathX.SmallestDegreeAngleBetween( 350.0f, 40.0f ), 0.00001f );
+        }
+
+        [Test]
+        public void NearlyEqualDouble()
+        {
+            Assert.IsTrue( MathX.NearlyEqual( 0.0, 0.0, 0.001 ) );
+            Assert.IsTrue( MathX.NearlyEqual( 1.0, 1.0, 0.001 ) );
+            Assert.IsTrue( MathX.NearlyEqual( 1.0001, 1.0001, 0.001 ) );
+            Assert.IsFalse( MathX.NearlyEqual( 1.0001, 1.010, 0.001 ) );
+        }
+
+        [Test]
+        public void NearlyEqualFloat()
+        {
+            Assert.IsTrue( MathX.NearlyEqual( 0.0f, 0.0f, 0.001f ) );
+            Assert.IsTrue( MathX.NearlyEqual( 1.0f, 1.0f, 0.001f ) );
+            Assert.IsTrue( MathX.NearlyEqual( 1.0001f, 1.0001f, 0.001f ) );
+            Assert.IsFalse( MathX.NearlyEqual( 1.0001f, 1.010f, 0.001f ) );
+        }
+
+        [Test]
+        public void FastSin()
+        {
+            const float pi = (float) Math.PI;
+
+            Assert.AreEqual( 0.0f, MathX.FastSin( 0.0f ) );
+            Assert.AreEqual( 0.707106f, MathX.FastSin( pi / 4.0f ), 0.001f );
+            Assert.AreEqual( 1.0f, MathX.FastSin( pi / 2.0f ), 0.001f );
+            Assert.AreEqual( 0.0f, MathX.FastSin( pi ), 0.001f );
+        }
+
+        [Test]
+        public void NextPowerOfTwoInt()
+        {
+            Assert.AreEqual( 1, MathX.NextPowerOf2( 0 ) );
+            Assert.AreEqual( 1, MathX.NextPowerOf2( 1 ) );
+            Assert.AreEqual( 2, MathX.NextPowerOf2( 2 ) );
+            Assert.AreEqual( 4, MathX.NextPowerOf2( 3 ) );
+            Assert.AreEqual( 4, MathX.NextPowerOf2( 4 ) );
+            Assert.AreEqual( 8, MathX.NextPowerOf2( 5 ) );
+            Assert.AreEqual( 65536, MathX.NextPowerOf2( 32769 ) );
+            Assert.AreEqual( -2147483648, MathX.NextPowerOf2( 2147483646 ) );
+        }
+
+        [Test]
+        public void NextPowerOfTwoUInt()
+        {
+            Assert.AreEqual( 1, MathX.NextPowerOf2( 0u ) );
+            Assert.AreEqual( 1, MathX.NextPowerOf2( 1u ) );
+            Assert.AreEqual( 2, MathX.NextPowerOf2( 2u ) );
+            Assert.AreEqual( 4, MathX.NextPowerOf2( 3u ) );
+            Assert.AreEqual( 4, MathX.NextPowerOf2( 4u ) );
+            Assert.AreEqual( 8, MathX.NextPowerOf2( 5u ) );
+            Assert.AreEqual( 65536, MathX.NextPowerOf2( 32769u ) );
+            Assert.AreEqual( 2147483648, MathX.NextPowerOf2( 2147483646u ) );
+            Assert.AreEqual( 0, MathX.NextPowerOf2( 2147483649U ) );
+        }
+
+        [Test]
+        public void NextPowerOfTwoLong()
+        {
+            Assert.AreEqual( 1, MathX.NextPowerOf2( 0L ) );
+            Assert.AreEqual( 1, MathX.NextPowerOf2( 1L ) );
+            Assert.AreEqual( 2, MathX.NextPowerOf2( 2L ) );
+            Assert.AreEqual( 4, MathX.NextPowerOf2( 3L ) );
+            Assert.AreEqual( 4, MathX.NextPowerOf2( 4L ) );
+            Assert.AreEqual( 8, MathX.NextPowerOf2( 5L ) );
+            Assert.AreEqual( 65536, MathX.NextPowerOf2( 32769L ) );
+            Assert.AreEqual( 2147483648, MathX.NextPowerOf2( 2147483646L ) );
+            Assert.AreEqual( -9223372036854775808, MathX.NextPowerOf2( 9223372036854775806L ) );
+        }
+
+        [Test]
+        public void NextPowerOfTwoULong()
+        {
+            Assert.AreEqual( 1, MathX.NextPowerOf2( 0UL ) );
+            Assert.AreEqual( 1, MathX.NextPowerOf2( 1UL ) );
+            Assert.AreEqual( 2, MathX.NextPowerOf2( 2UL ) );
+            Assert.AreEqual( 4, MathX.NextPowerOf2( 3UL ) );
+            Assert.AreEqual( 4, MathX.NextPowerOf2( 4UL ) );
+            Assert.AreEqual( 8, MathX.NextPowerOf2( 5UL ) );
+            Assert.AreEqual( 65536, MathX.NextPowerOf2( 32769UL ) );
+            Assert.AreEqual( 2147483648, MathX.NextPowerOf2( 2147483646UL ) );
+            Assert.AreEqual( 9223372036854775808, MathX.NextPowerOf2( 9223372036854775806UL ) );
+            Assert.AreEqual( 0, MathX.NextPowerOf2( 18446744073709551615UL ) );
+        }
+
+        [Test]
+        public void LerpFloat()
+        {
+            Assert.AreEqual( 1.0f, MathX.Lerp( 2.0f, 4.0f, -0.5f ) );
+            Assert.AreEqual( 2.0f, MathX.Lerp( 2.0f, 4.0f, 0.00f ) );
+            Assert.AreEqual( 2.5f, MathX.Lerp( 2.0f, 4.0f, 0.25f ) );
+            Assert.AreEqual( 3.0f, MathX.Lerp( 2.0f, 4.0f, 0.50f ) );
+            Assert.AreEqual( 3.5f, MathX.Lerp( 2.0f, 4.0f, 0.75f ) );
+            Assert.AreEqual( 4.0f, MathX.Lerp( 2.0f, 4.0f, 1.00f ) );
+            Assert.AreEqual( 6.0f, MathX.Lerp( 2.0f, 4.0f, 2.00f ) );
+        }
+
+        [Test]
+        public void LerpDouble()
+        {
+            Assert.AreEqual( 1.0, MathX.Lerp( 2.0, 4.0, -0.5 ) );
+            Assert.AreEqual( 2.0, MathX.Lerp( 2.0, 4.0, 0.00 ) );
+            Assert.AreEqual( 2.5, MathX.Lerp( 2.0, 4.0, 0.25 ) );
+            Assert.AreEqual( 3.0, MathX.Lerp( 2.0, 4.0, 0.50 ) );
+            Assert.AreEqual( 3.5, MathX.Lerp( 2.0, 4.0, 0.75 ) );
+            Assert.AreEqual( 4.0, MathX.Lerp( 2.0, 4.0, 1.00 ) );
+            Assert.AreEqual( 6.0, MathX.Lerp( 2.0, 4.0, 2.00 ) );
+        }
+
+        [Test]
+        public void UnlerpFloat()
+        {
+            Assert.AreEqual( -0.5f, MathX.Unlerp( 2.0f, 4.0f, 1.00f ) );
+            Assert.AreEqual( 0.00f, MathX.Unlerp( 2.0f, 4.0f, 2.00f ) );
+            Assert.AreEqual( 0.25f, MathX.Unlerp( 2.0f, 4.0f, 2.50f ) );
+            Assert.AreEqual( 0.50f, MathX.Unlerp( 2.0f, 4.0f, 3.00f ) );
+            Assert.AreEqual( 0.75f, MathX.Unlerp( 2.0f, 4.0f, 3.50f ) );
+            Assert.AreEqual( 1.00f, MathX.Unlerp( 2.0f, 4.0f, 4.00f ) );
+            Assert.AreEqual( 2.00f, MathX.Unlerp( 2.0f, 4.0f, 6.00f ) );
+        }
+
+        [Test]
+        public void UnlerpDouble()
+        {
+            Assert.AreEqual( -0.5, MathX.Unlerp( 2.0, 4.0, 1.00 ) );
+            Assert.AreEqual( 0.00, MathX.Unlerp( 2.0, 4.0, 2.00 ) );
+            Assert.AreEqual( 0.25, MathX.Unlerp( 2.0, 4.0, 2.50 ) );
+            Assert.AreEqual( 0.50, MathX.Unlerp( 2.0, 4.0, 3.00 ) );
+            Assert.AreEqual( 0.75, MathX.Unlerp( 2.0, 4.0, 3.50 ) );
+            Assert.AreEqual( 1.00, MathX.Unlerp( 2.0, 4.0, 4.00 ) );
+            Assert.AreEqual( 2.00, MathX.Unlerp( 2.0, 4.0, 6.00 ) );
+        }
+    }
+}
