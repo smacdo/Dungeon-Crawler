e2d525f2967abfe5c71117be2aa48209d3aa0281
Scott MacDonald <scott@smacdo.com>
2017-06-13 23:08:01 -0700

    Initial implementation of A* pathfinder. A lot more work remains to getting it into a shipping state.

---
+++ src/Scott.Forge.Tests/Tilemaps/GridPathfinderTests.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scott.Forge.Tilemaps;
+using System.Collections.Generic;
+
+namespace Scott.Forge.Tests.Tilemaps
+{
+    [TestClass]
+    public class GridPathfinderTests
+    {
+        [TestMethod]
+        public void Pathfind_When_Goal_Is_Start()
+        {
+            var grid = CreateGrid(3, 3, new int[,] {
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 }
+            });
+
+            var result = Pathfind_ManhattanDistance(new Point2(1, 1), new Point2(1, 1), grid);
+            CollectionAssert.AreEqual(new Point2[] {}, result);
+        }
+
+        [TestMethod]
+        public void Pathfind_When_Goal_Is_One_Cell_Away_In_All_Directions()
+        {
+            var grid = CreateGrid(3, 3, new int[,] {
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 }
+            });
+
+            // North.
+            var result = Pathfind_ManhattanDistance(new Point2(1, 1), new Point2(1, 0), grid);
+            CollectionAssert.AreEqual(new Point2[] { new Point2(1, 0) }, result);
+
+            // East.
+            result = Pathfind_ManhattanDistance(new Point2(1, 1), new Point2(2, 1), grid);
+            CollectionAssert.AreEqual(new Point2[] { new Point2(2, 1) }, result);
+
+            // South.
+            result = Pathfind_ManhattanDistance(new Point2(1, 1), new Point2(1, 2), grid);
+            CollectionAssert.AreEqual(new Point2[] { new Point2(1, 2) }, result);
+
+            // West.
+            result = Pathfind_ManhattanDistance(new Point2(1, 1), new Point2(0, 1), grid);
+            CollectionAssert.AreEqual(new Point2[] { new Point2(0, 1) }, result);
+        }
+
+        [TestMethod]
+        public void Pathfind_From_Top_Left_To_Arbitrary_Cell_Multiple_Cells_Away()
+        {
+            var grid = CreateGrid(3, 3, new int[,] {
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 }
+            });
+
+            var result = Pathfind_ManhattanDistance(new Point2(0, 0), new Point2(2, 0), grid);
+            CollectionAssert.AreEqual(new Point2[] { new Point2(1, 0), new Point2(2, 0) }, result);
+
+            result = Pathfind_ManhattanDistance(new Point2(0, 0), new Point2(0, 2), grid);
+            CollectionAssert.AreEqual(new Point2[] { new Point2(0, 1), new Point2(0, 2) }, result);
+
+            // Two solutions but path finder prefers to go east first.
+            result = Pathfind_ManhattanDistance(new Point2(0, 0), new Point2(1, 1), grid);
+            CollectionAssert.AreEqual(new Point2[] { new Point2(1, 0), new Point2(1, 1) }, result);
+
+            // Two solutions but path finder prefers to go east first.
+            result = Pathfind_ManhattanDistance(new Point2(0, 0), new Point2(2, 2), grid);
+            CollectionAssert.AreEqual(new Point2[]
+            {
+                new Point2(1, 0), new Point2(1, 1), new Point2(2, 1), new Point2(2, 2)
+            }, result);
+        }
+
+
+        [TestMethod]
+        public void Pathfind_From_Top_Left_To_Arbitrary_Cell_Multiple_Cells_Away_With_Cost()
+        {
+            var grid = CreateGrid(3, 3, new int[,] {
+                { 1, 6, 1 },
+                { 1, 3, 1 },
+                { 1, 1, 1 }
+            });
+
+            var result = Pathfind_ManhattanDistance(new Point2(0, 0), new Point2(2, 0), grid);
+            CollectionAssert.AreEqual(new Point2[]
+            {
+                new Point2(0, 1),
+                new Point2(1, 1),
+                new Point2(2, 1),
+                new Point2(2, 0)
+            }, result);
+
+            /*
+            result = Pathfind_ManhattanDistance(new Point2(0, 0), new Point2(0, 2), grid);
+            CollectionAssert.AreEqual(new Point2[] { new Point2(0, 1), new Point2(0, 2) }, result);
+
+            // Two solutions but path finder prefers to go east first.
+            result = Pathfind_ManhattanDistance(new Point2(0, 0), new Point2(1, 1), grid);
+            CollectionAssert.AreEqual(new Point2[] { new Point2(1, 0), new Point2(1, 1) }, result);
+
+            // Two solutions but path finder prefers to go east first.
+            result = Pathfind_ManhattanDistance(new Point2(0, 0), new Point2(2, 2), grid);
+            CollectionAssert.AreEqual(new Point2[]
+            {
+                new Point2(1, 0), new Point2(1, 1), new Point2(2, 1), new Point2(2, 2)
+            }, result);*/
+        }
+
+        // TODO: Test diagonal.
+        // TODO: Test where solution could not be found.
+
+        private Point2[] Pathfind_ManhattanDistance(Point2 start, Point2 end, Grid<int> grid)
+        {
+            var pf = new GridPathfinder<int>(grid, GetExactMovementCost, GetManhattanDistance);
+            return ToArray(pf.CalculatePath(start, end));
+        }
+
+        private T[] ToArray<T>(IEnumerable<T> enumerable)
+        {
+            var result = new List<T>();
+            foreach (var v in enumerable)
+            {
+                result.Add(v);
+            }
+
+            return result.ToArray();
+        }
+        
+        private Grid<int> CreateGrid(int rows, int cols, int[,] values)
+        {
+            var grid = new Grid<int>(cols, rows);
+
+            if (values == null)
+            {
+                grid.Fill((Grid<int> g, int x, int y) => { return 0; });
+            }
+            else
+            {
+                for (int y = 0; y < values.GetLength(0); y++)
+                {
+                    for (int x = 0; x < values.GetLength(1); x++)
+                    {
+                        grid[x, y] = values[y, x];
+                    }
+                }
+            }
+
+            return grid;
+        }
+
+        private float GetManhattanDistance(Grid<int> grid, Point2 from, Point2 goal)
+        {
+            const float MovementCost = 1.0f;
+
+            var dx = Math.Abs(from.X - goal.X);
+            var dy = Math.Abs(from.Y - goal.Y);
+
+            return MovementCost * (dx + dy);
+        }
+
+        private float GetExactMovementCost(Grid<int> grid, Point2 from, Point2 to)
+        {
+            // TODO: Assert from -> to is only one tile away.
+            // TODO: Assert from -> matches allowed movement (straight or diagonals).
+            return Point2.Distance(from, to) * grid.Cells[to.X, to.Y];
+        }
+    }
+}
---
+++ src/Scott.Forge/Tilemaps/GridPathfinder.cs
@@ -0,0 +1,251 @@
+﻿/*
+ * Copyright 2017 Scott MacDonald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using Scott.Forge.Support;
+using System;
+using System.Collections.Generic;
+
+namespace Scott.Forge.Tilemaps
+{
+    public delegate float PathCostDelegate<CellType>(Grid<CellType> grid, Point2 from, Point2 to);
+
+    /// <summary>
+    ///  A customizable 2d grid based path finder based on A-Star.
+    ///  TODO: Needs a lot of optimization, right now allocates a lot of stuff.
+    /// </summary>
+    public class GridPathfinder<CellType>
+    {
+        private PriorityQueue<Point2, float> mFrontier;
+        private Grid<PathCell> mCells;
+
+        /// <summary>
+        ///  Constructor.
+        /// </summary>
+        /// <param name="grid">Grid to path find over.</param>
+        /// <param name="actualCostDelegate">TODO</param>
+        /// <param name="estimatedCostDelegate">TODO</param>
+        public GridPathfinder(
+                Grid<CellType> grid,
+                PathCostDelegate<CellType> actualCostDelegate,
+                PathCostDelegate<CellType> estimatedCostDelegate)
+        {
+            if (grid == null)
+            {
+                throw new ArgumentNullException(nameof(grid));
+            }
+
+            if (actualCostDelegate == null)
+            {
+                throw new ArgumentNullException(nameof(actualCostDelegate));
+            }
+
+            if (estimatedCostDelegate == null)
+            {
+                throw new ArgumentNullException(nameof(estimatedCostDelegate));
+            }
+
+            Grid = grid;
+            ActualCostDelegate = actualCostDelegate;
+            EstimatedCostDelegate = estimatedCostDelegate;
+
+            mFrontier = new PriorityQueue<Point2, float>(EstimateMaxOpenNodeCount(Grid));
+            mCells = new Grid<PathCell>(Grid.Cols, Grid.Rows);
+        }
+
+        public Grid<CellType> Grid { get; private set; }
+
+        public PathCostDelegate<CellType> ActualCostDelegate { get; set; }
+
+        public PathCostDelegate<CellType> EstimatedCostDelegate { get; set; }
+
+        public bool AllowDiagonalAdjacency { get; set; }
+
+        public IEnumerable<Point2> CalculatePath(Point2 start, Point2 goal)
+        {
+            mFrontier.Clear();
+            mCells.Clear();
+
+            mFrontier.Add(start, 0);
+            mCells[start].MakeOpen();
+
+            bool didFindPath = false;
+
+            while (!mFrontier.IsEmpty)
+            {
+                var currentPosition = mFrontier.Remove();
+                
+                // Is this the goal?
+                if (currentPosition == goal)
+                {
+                    didFindPath = true;
+                    break;
+                }
+
+                // Visit straight edge neighbors.
+                VisitNeighbors(currentPosition);
+
+                // Set current cell to be closed now that all neighbors have been inspected.
+                mCells[currentPosition].MakeClosed();
+            }
+
+            // TODO: Handle where path could not be generated.
+
+            // Calculate a path from the start to the goal and return it.
+            return didFindPath ? GetPathToStart(start, goal) : null;
+        }
+
+        private List<Point2> GetPathToStart(Point2 start, Point2 goal)
+        {
+            var selectedPath = new List<Point2>();      // TOD: Presize and maybe use a pool.
+            //selectedPath.Add(goal);
+
+            var currentPosition = goal;
+
+            while (currentPosition != start)
+            {
+                selectedPath.Add(currentPosition);
+                currentPosition = mCells[currentPosition].CameFrom;
+            }
+
+            // TODO: Add test for infinite following (when grid is broken).
+
+            //selectedPath.Add(start);    // TODO: Optional, not sure if best.
+            selectedPath.Reverse();
+
+            return selectedPath;
+        }
+
+        private void VisitNeighbors(Point2 position)
+        {
+            // East neighbor.
+            if (position.X < Grid.Cols - 1)
+            {
+                VisitNeighbor(position, new Point2(1, 0));
+            }
+
+            // South.
+            if (position.Y < Grid.Rows - 1)
+            {
+                VisitNeighbor(position, new Point2(0, 1));
+            }
+
+            // West.
+            if (position.X > 0)
+            {
+                VisitNeighbor(position, new Point2(-1, 0));
+            }
+
+            // North.
+            if (position.Y > 0)
+            {
+                VisitNeighbor(position, new Point2(0, -1));
+            }
+        }
+
+        private void VisitNeighbor(Point2 position, Point2 offset)
+        {
+            var neighborPosition = position + offset;
+
+            // Get the cost of moving from the start to this tile (the actual cost).
+            var moveCost = mCells[position].BestCost;
+            moveCost += ActualCostDelegate(Grid, position, neighborPosition);
+
+            // Check if this node is currently open with a larger cost. If true then replace the entry with this entry
+            // in the open set.
+
+            // ...
+            var cell = mCells[neighborPosition];
+
+            if (moveCost < cell.BestCost)
+            {
+                if (cell.IsOpen)
+                {
+                    cell.State = CellState.Default;
+                    mFrontier.Remove(neighborPosition);
+                }
+                else if (cell.IsClosed)
+                {
+                    // This should not happen with a consistent admissible heuristic.
+                    // TODO: Remove neighbor from closed.
+                    cell.State = CellState.Default;
+                }
+            }
+            
+            // ...
+            if (cell.IsClosed == false && cell.IsOpen == false)
+            {
+                cell.BestCost = moveCost;
+                cell.MakeOpen();
+
+                var estimatedCost = EstimatedCostDelegate(Grid, position, neighborPosition);
+                var newScore = moveCost + estimatedCost;
+
+                mFrontier.Add(neighborPosition, newScore);
+
+                cell.CameFrom = position;
+            }
+
+            // ...
+            mCells[neighborPosition] = cell;
+        }
+
+        private int EstimateMaxOpenNodeCount(Grid<CellType> grid)
+        {
+            // TODO: Be smarter about this.
+            return grid.Count / 4;
+        }
+
+        private int EstimateMaxClosedNodeCount(Grid<CellType> grid)
+        {
+            // TODO: Be smarter about this.
+            return grid.Count / 4;
+        }
+
+        private enum CellState
+        {
+            Default,
+            Open,
+            Closed
+        }
+
+        private struct PathCell
+        {
+            public CellState State;
+            public Point2 CameFrom;
+            public float BestCost;
+
+            public void MakeOpen()
+            {
+                State = CellState.Open;
+            }
+
+            public void MakeClosed()
+            {
+                State = CellState.Closed;
+            }
+            
+            public bool IsOpen
+            {
+                get { return State == CellState.Open; }
+            }
+
+            public bool IsClosed
+            {
+                get { return State == CellState.Closed; }
+            }
+        }
+        
+    }
+}
